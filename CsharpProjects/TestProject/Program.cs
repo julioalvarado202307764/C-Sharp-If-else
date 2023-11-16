54a7a61168986cfab806218375b5251d8036fb13
Julio Eduardo Gustavo Alvarado Ramirez <128263541+julioalvarado202307764@users.noreply.github.com>
2023-11-16 11:59:27 -0600

    Update Program.cs

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -26,3 +26,31 @@ else if(daysUntilExpiration <= 10)
 if(discountPercentage > 0){
     Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
+/*if (daysUntilExpiration == 0) //Esta otra forma asigno el porcentaje dentro del if de días de expiración
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+    if (discountPercentage > 0)
+    {
+        Console.WriteLine($"Renew now and save {discountPercentage}%!");
+    }
+}
+else if (daysUntilExpiration <= 5)
+{
+
+    Console.WriteLine($"Your subscription expired in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+    if (discountPercentage > 0)
+    {
+        Console.WriteLine($"Renew now and save {discountPercentage}%!");
+    }
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+*/
